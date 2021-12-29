f7e1c953c0eb47aecde23eeb3ab96d2e28a0c316
MasoudRabiee <masoud.rabiee20@gmail.com>
2021-12-29 16:55:38 +0330

    Add listener for digits Buttons

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,11 +2,62 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var dotFlag = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_0.setOnClickListener(digitListener)
+        btn_1.setOnClickListener(digitListener)
+        btn_2.setOnClickListener(digitListener)
+        btn_3.setOnClickListener(digitListener)
+        btn_4.setOnClickListener(digitListener)
+        btn_5.setOnClickListener(digitListener)
+        btn_6.setOnClickListener(digitListener)
+        btn_7.setOnClickListener(digitListener)
+        btn_8.setOnClickListener(digitListener)
+        btn_9.setOnClickListener(digitListener)
+        btn_0.setOnClickListener(digitListener)
+        btn_dot.setOnClickListener(digitListener)
+        btn_sign.setOnClickListener(digitListener)
+    }
+
+
+    private val digitListener = View.OnClickListener {
+        it as Button
+        val oldNumber = text_display.text.toString()
+        when(it.id){
+            btn_dot.id -> {
+                if (!dotFlag){
+                    text_display.append(".")
+                    dotFlag = true
+                }
+            }
+            btn_sign.id -> {
+                if (oldNumber.first() == '-'){
+                    val newNumber = oldNumber.drop(1)
+                    text_display.setText(newNumber)
+                }
+                else if (oldNumber != "0" && oldNumber.isNotEmpty()){
+                    text_display.setText("-$oldNumber")
+                }
+            }
+            else ->{
+                if (oldNumber == "0" || oldNumber.isEmpty()){
+                    text_display.setText(it.text)
+                }
+                else{
+                    text_display.append(it.text)
+                }
+            }
+        }
     }
 }
+
